ccd05eae2079fe3e0b6cb60d1bec2159899d5b0a
jjjohannn <johan19_98@hotmail.com>
2022-04-24 21:18:35 -0400

    cambio de puerto

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 app.get('/',(req,res)=>res.send ('hola un nuevo mensaje para people '))
-app.listen(80);
-console.log("server on port 80")
+app.listen(8080);
+console.log("server on port 8080")
