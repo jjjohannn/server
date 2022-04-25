e8fa2472262156716fff56769c7c7d49ff47988c
jjjohannn <johan19_98@hotmail.com>
2022-04-24 20:59:15 -0400

    mensaje editado

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
-app.get('/',(req,res)=>res.send ('hola'))
+app.get('/',(req,res)=>res.send ('hola un nuevo mensaje para people '))
 app.listen(3000);
 console.log("server on port 3000")
